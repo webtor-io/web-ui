f58bde483aa2bffbfecae98c011e8eb8cd155a89
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2025-08-25 19:17:18 +0300

    update 14-th migration

--- migrations/14_populate_library_names.down.sql
+++ migrations/14_populate_library_names.down.sql
@@ -1,6 +1,1 @@
--- Revert library.name to NULL where it was populated from torrent_resource.name
-UPDATE public.library l
-SET name = NULL 
-FROM public.torrent_resource tr
-WHERE l.resource_id = tr.resource_id 
-  AND l.name = tr.name;
+-- do nothing here
